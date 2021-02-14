eb16b07b7faa378feee28809dc96afb83cf1eb21
Louis Young <me@louisyoung.co.uk>
2021-02-14 12:39:42 +0000

    Add native OS dark mode toggle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Footer from "./footer";
 import Header from "./header";
 import Markdown from "./markdown";
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
   return (
-    <section className="dark">
-      <div className="dark:bg-dark dark:text-white">
-        <Header />
-        <main>
-          <Markdown>{children}</Markdown>
-        </main>
-        <Footer />
-      </div>
+    <section className="dark:bg-dark dark:text-white">
+      <Header />
+      <main>
+        <Markdown>{children}</Markdown>
+      </main>
+      <Footer />
     </section>
   );
 };
