7f12eeb3d08e6e255847a4c44d51f592fc187bd2
Louis Young <me@louisyoung.co.uk>
2021-02-17 17:44:50 +0000

    Personalise 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
       <Meta title="Oops - Page Not Found | Louis Young" />
       <section>
         <h1 className="py-20 text-center text-2xl md:text-4xl font-bold tracking-tight leading-normal md:leading-normal">
-          Oops, we couldn't find what you
+          Oops, I couldn't find what you
           <br /> were looking for.
         </h1>
       </section>
